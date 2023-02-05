b4fcf370acd0edc95289056f1e8015fc88416ba2
Echesa-Imaculate-Zepwa <global-imaculate.zepwa@student.moringaschool.com>
2023-02-05 11:27:58 +0300

    New angular componet and created a navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from 'src/navbar/navbar.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
